2c4f2946de907264ea3bae3334d08373c572f9af
sun00K <ssuno0ki3@gmail.com>
2022-12-28 23:54:45 +0900

    Button Design

--- src/components/css/StyleButton.js
+++ src/components/css/StyleButton.js
@@ -13,11 +13,13 @@ const NavButton = styled.button`
     height: 30px;
     color: ${primary};
     border: 2px solid ${primary};
-    border-radius: 1em;
+    border-radius: 23px;
     margin: 0px 12px ;
     background: none;
     padding-left: 18px;
     padding-right: 18px;
+
+    font: normal 700 15px/22px ITC Avant Garde Gothic Pro, sans-serif;
 
     &:hover{
         color: '#FFF';
